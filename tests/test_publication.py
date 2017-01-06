aa4aa7bf8c24f3068e562a0031b386a0d3a7af60
prmurphy <prmurphy27@gmail.com>
2017-01-06 00:41:36 -0600

    Update test_publication.py

--- tests/test_publication.py
+++ tests/test_publication.py
@@ -15,6 +15,11 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
+def checkJquery(driver):    
+    JqueryActive = driver.execute_script("return jQuery.active")
+    while JqueryActive != 0:
+        sleep(0.5)
+        JqueryActive = driver.execute_script("return jQuery.active")
 
 @on_platforms(browsers)
 class TestPublication(BaseCase):
@@ -75,22 +80,28 @@ class TestPublication(BaseCase):
         """Clicks on the 'Manager' text and checks to see that
            the URL is correct."""
         
-        sleep(5)
+        
         driver = self.driver
-        input_element = driver.find_element_by_id('NationManagerLink')
+        
+        input_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="NationManagerLink"]'))) 
+        checkJquery(driver);
         input_element.click()
-        sleep(5)        
+        input_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Publications")))       
         expected = 'http://127.0.0.1:5981/apps/_design/bell/nation/index.html#dashboard'
         actual = driver.current_url
+        driver.implicitly_wait(10)
         self.assertEqual(actual, expected)
         
     def click_publications_test(self):
         """Clicks on 'publications' and checks URL"""
-        sleep(5)
+       
+        
         driver = self.driver
-        input_element = driver.find_element_by_partial_link_text('Publications')
+        checkJquery(driver);
+        input_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="publications"]/tbody/tr[1]/th/a')))
         input_element.click()
-        sleep(2)
+        input_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Add Issue")))
+        driver.implicitly_wait(10)
         expected = 'http://127.0.0.1:5981/apps/_design/bell/nation/index.html#publication'
         actual = driver.current_url
         self.assertEqual(actual, expected)
@@ -98,14 +109,19 @@ class TestPublication(BaseCase):
     def click_and_add_issue(self):
         """Clicks 'add issue' button, checks URL then adds an issue which it checks by reaing the
            popup text"""
-        sleep(5)
+        
         driver = self.driver
-        input_element = driver.find_element_by_partial_link_text('Add Issue')
+        checkJquery(driver);
+        input_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Add Issue")))
+        #input_element = driver.find_element_by_partial_link_text('Add Issue')
         input_element.click()
-        sleep(2)
+        #this is where I have stopped 1/5/2017 4:30
+        input_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "editorName")))
         expected = 'http://127.0.0.1:5981/apps/_design/bell/nation/index.html#publication/add'
         actual = driver.current_url
         self.assertEqual(actual, expected)
+
+        checkJquery(driver);
         input_element = driver.find_element_by_name('editorName')
         input_element.send_keys('Test Name')
         input_element = driver.find_element_by_name('editorEmail')
@@ -114,29 +130,31 @@ class TestPublication(BaseCase):
         input_element.send_keys('1111111')
         input_element = driver.find_element_by_name('save')
         input_element.click()
-        sleep(3)
-        alert = driver.switch_to_alert()
+
+        element = WebDriverWait(driver, 10).until(EC.alert_is_present(),'Issue saved')
+        alert = driver.switch_to.alert
         actual = alert.text
         expected = 'Issue saved.'        
         self.assertEqual(actual, expected)
         alert.accept()
-        sleep(3)
+        checkJquery(driver);
         input_element = driver.find_element_by_partial_link_text('My Home')
         input_element.click()
-        sleep(5)    
+           
 
     def delete_test(self):
         """Clicks the first element with 'Delete' as text then checks deletion by reaing popup"""
-        sleep(5)
         driver = self.driver
+        checkJquery(driver);
+        input_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Delete")))        
         input_element = driver.find_element_by_xpath(".//*[contains(text(), 'Delete')]")
         input_element.click()
-        alert = driver.switch_to_alert()
+        alert = driver.switch_to.alert
         actual = alert.text
         expected = 'Are you sure you want to delete this publication?'
         alert.accept()
-        sleep(1)
-        alert = driver.switch_to_alert()
+        element = WebDriverWait(driver, 10).until(EC.alert_is_present(),'Model of publications is accessed.')
+        alert = driver.switch_to.alert
         alert.accept()
 
     
